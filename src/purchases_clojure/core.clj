3051781ebad81d81d498ae75918255c5d8f47582
carolineevail <carolineevail@gmail.com>
2016-03-22 15:08:45 -0400

    Finished

--- src/purchases_clojure/core.clj
+++ src/purchases_clojure/core.clj
@@ -1,5 +1,26 @@
 (ns purchases-clojure.core
+  (:require [clojure.string :as str]
+            [clojure.walk :as walk])
   (:gen-class))
 
+
 (defn -main []
-  (println "Hello, World!"))
+  (println "Type a category [Furniture, Alcohol, Toiletries, Shoes, Food, Jewelry] and hit enter to view all purchases in that category.")
+  (let [purchases (slurp "purchases.csv")
+        text (read-line)
+        purchases (str/split-lines purchases)
+        purchases (map (fn [line]
+                         (str/split line #","))
+                      purchases)
+        header (first purchases)
+        purchases (rest purchases)
+        purchases (map (fn [line]
+                         (apply hash-map (interleave header line)))
+                       purchases)
+        purchases (walk/keywordize-keys purchases)
+        purchases (filter (fn [line]
+                            (= (:category line) text))
+                       purchases)]
+    (spit "filtered_purchases.edn" (pr-str purchases))
+    purchases))
+
